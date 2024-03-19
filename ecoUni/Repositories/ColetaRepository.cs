c381b55628703246c977111eff9540e531620bf0
Kaique Eduardo <zoomkaique1528@gmail.com>
2024-03-18 21:44:12 -0300

    criacao repositorios e interface

---
+++ ecoUni/Repositories/ColetaRepository.cs
@@ -0,0 +1,18 @@
+﻿using ecoUni.Context;
+using ecoUni.Models;
+using ecoUni.Repositories.Interfaces;
+
+namespace ecoUni.Repositories
+{
+    public class ColetaRepository : IColetaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ColetaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Coleta> Coletas => _context.Coletas;
+    }
+}
---
+++ ecoUni/Repositories/Interfaces/IColetaRepository.cs
@@ -0,0 +1,9 @@
+﻿using ecoUni.Models;
+
+namespace ecoUni.Repositories.Interfaces
+{
+    public interface IColetaRepository
+    {
+        IEnumerable<Coleta> Coletas { get; }
+    }
+}
